3132648ea8172d7fe7da953eaf86f40f125fbfc9
AlexanderAbkh <alexabkh155@gmail.com>
2022-12-14 18:18:13 +0300

    updated all files

---
+++ env_config.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+FULL_PATH = os.path.dirname(os.path.realpath(__file__))
+
+cred_path = str(FULL_PATH)+'\\creds.json'
+creds_file = open(cred_path, 'rb')
+creds = json.load(creds_file)
+
+
+
+USERNAME = creds['correct_username']
+PASSWORD = creds['correct_password']
+INCORRECT_PASSWORD = creds['incorrect_password']
