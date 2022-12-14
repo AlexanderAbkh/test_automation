c08eb7ce3fff9f5da1d5f1c1614e6861d57091aa
AlexanderAbkh <alexabkh155@gmail.com>
2022-12-14 19:03:07 +0300

    fixed path

--- env_config.py
+++ env_config.py
@@ -3,7 +3,7 @@ import json
 
 FULL_PATH = os.path.dirname(os.path.realpath(__file__))
 
-cred_path = str(FULL_PATH)+'\\creds.json'
+cred_path = str(FULL_PATH)+'\\test_data\\creds.json'
 creds_file = open(cred_path, 'rb')
 creds = json.load(creds_file)
 
